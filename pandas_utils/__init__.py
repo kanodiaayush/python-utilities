eef5b27f306bf641213ef08fc38dee01b672366f
Ayush Kanodia <kanodiaayush@gmail.com>
2021-07-16 22:12:19 +0000

    add config files

---
+++ pandas_utils/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Pandas Utility"""
+
+__author__ = "Ayush Kanodia, Keshav Agrawal"
+__email__ = "kanodiaayush@gmail.com, kasince2k@gmail.com"
+
+from pandas_utils.utils import *  # noqa:401
